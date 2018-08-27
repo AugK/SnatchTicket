f6c4d07080b41091de2c7ab0bf7dca0f4f4ddfc5
kjy666 <tckjy1990@gmail.com>
2018-08-28 07:58:34 +0800

    add success_flag

--- pkg_demo/demo.py
+++ pkg_demo/demo.py
@@ -6,6 +6,9 @@ import datetime
 import random
 
 
+success_flag = False
+
+
 def login(b, username, password):   # 登录账号
     b.click_link_by_text(u"登录")
     b.fill("username", username)
@@ -27,7 +30,7 @@ def select_submit(b, choice):
         select_submit(b, choice)
 
 
-def loop_popup(b, choice):  # 循环点击
+def loop_popup(b, choice, url):  # 循环点击
     try:
         if b.is_element_present_by_value("确认预订"):
             b.click_link_by_id("btn_submit")  # click"确认预订"
@@ -36,6 +39,7 @@ def loop_popup(b, choice):  # 循环点击
                 b.click_link_by_id("popup_ok")  # click"确认"
 
                 pop_msg = ""
+                global success_flag
 
                 if b.is_element_present_by_id("popup_message", 30):
                     pop_msg = b.find_by_id("popup_message").text  # 获取popup message
@@ -44,43 +48,46 @@ def loop_popup(b, choice):  # 循环点击
                 if "成功" in pop_msg:
                     b.click_link_by_id("popup_ok")
                     print("已预订成功，请等待短信通知^_^")
+                    success_flag = True
 
                 elif "当前时间不可预定" in pop_msg or len(pop_msg) > 100:
                     b.click_link_by_id("popup_ok")
                     select_submit(b, choice)
-                    loop_popup(b, choice)
+                    loop_popup(b, choice, url)
 
-                elif "预订已满" in pop_msg:
+                elif "已" in pop_msg: # "预订已满", "您已预定当前场次"
                     print("别瞎忙活了，票没了")
+                    success_flag = True
 
                 elif "默认泳池" in pop_msg:
                     b.click_link_by_id("popup_ok")  # not sure
 
                 else:
-                    print(pop_msg + "line66：unknow situation")
+                    print(pop_msg + "\n\tline66：unknow situation")
                     b.reload()
                     select_submit(b, choice)
-                    loop_popup(b, choice)
+                    loop_popup(b, choice, url)
 
             else:
                 print("no response, try again")
-                b.reload()
+                b.visit(url)
                 select_submit(b, choice)
-                loop_popup(b, choice)
+                loop_popup(b, choice, url)
 
         elif b.is_element_present_by_text("已订完"):
             print("已订完，明天再试吧！")
+            success_flag = True
 
         else:
             print("line81: unknown situation")
-            b.reload()
+            b.visit(url)
             select_submit(b, choice)
-            loop_popup(b, choice)
+            loop_popup(b, choice, url)
 
-    except splinter.exceptions.ElementDoesNotExist as e:
-        b.reload()
+    except Exception as e:
+        b.visit(url)
         select_submit(b, choice)
-        loop_popup(b, choice)
+        loop_popup(b, choice, url)
         print(e)
 
 
@@ -93,7 +100,6 @@ def run(user, password, url="http://www.wentiyun.cn/venue-722.html", choice="2",
     # start/end time
     start_time = datetime.datetime(year, mon, day, 7, 0, 0, random.randint(0, 5))
     end_time = datetime.datetime(year, mon, day, 7, 1, 0, 0)
-    halt_time = datetime.datetime(year, mon, day, 7, 2, 0)
 
     # purpose
     print("今天抢星期", today.tm_wday + int(choice), "的票")
@@ -109,14 +115,13 @@ def run(user, password, url="http://www.wentiyun.cn/venue-722.html", choice="2",
     # start snatching
     while True:
         now = datetime.datetime.now()
-        if start_time < now < end_time:
+        if start_time < now < end_time and not success_flag:
             print("start snatching...")
             print(now)
-            # b.reload()
-            loop_popup(b, choice)
-        elif now > halt_time:
+            loop_popup(b, choice, url)
+        else:
             print(now)
-            print("=========OVER TIME=========")
+            print("=========OVER TIME=========") if success_flag else print("=========OVER TIME=========")
             break
 
 
